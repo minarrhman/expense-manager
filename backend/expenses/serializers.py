df155f9651c35e8d3375035dcee76d6e17ff9b0d
minar <minar.rhman@gmail.com>
2026-04-22 04:30:52 +0600

    updated views

--- backend/expenses/serializers.py
+++ backend/expenses/serializers.py
@@ -4,5 +4,20 @@ from .models import Category, Transaction
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
-        
+        fields = ['id', 'name', 'type']
+
+class TransactionSerializer(serializers.ModelSerializer):
+    category_name = serializers.Charfield(source='category.name', read_only=True)
+    class Meta:
+        model = Transaction
+        fields = ['id', 'amount',  'type', 'category', 'category_name' ,'date', 'description']
+
+    def validate(self, data):
+        category = data.get('category')
+        type_ = data.get('type')
+
+        if category != type_:
+            raise serializers.ValidationError(
+                "Transaction type and category do not match"
+            )
+        return data
--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import isAuthenticated
+from django.db.models import Q
+
+from .models import Category, Transaction
+from .serializers import CategorySerializer, TransactionSerializer
 
 # Create your views here.
+
+class CategoryListCreateView(generics.ListCreateView):
+    serialzerclass = CategorySerializer
+    permission_classes = [isAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        return Category.objects.filter(
+            Q(user=user) | Q(user__isnull=True)
+        )
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TransactionListCreateView(generics.ListCreateView):
+    serializer = TransactionSerializer
+    permission_classes = [isAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(
+            user=self.request.user
+        ).order_by('-date')
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
