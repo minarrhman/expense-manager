a4ebe56164a119847da25b847253d543296628a4
minar <minar.rhman@gmail.com>
2026-04-22 03:24:08 +0600

    added serializer

---
+++ backend/expenses/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Category, Transaction
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        
