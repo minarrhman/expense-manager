1c84655b6269c545835d287390be8140ff82c5f6
minar <minar.rhman@gmail.com>
2026-05-13 04:44:21 +0600

    updated limit api

--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -38,6 +38,28 @@ class CategoryLimit(models.Model):
     def __str__(self):
         return f"{self.user} - {self.category} limit"
 
+class SavingsModel(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+
+    target_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    current_amount = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+
+    start_date = models.DateField(auto_now_add=True)
+    target_date = models.DateField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def progress_percentage(self):
+        if self.target_amount == 0:
+            return 0
+        return min(
+            (self.current_amount/self.target_amount)*100,
+        100)
+    
 
 class Transaction(models.Model):
     TYPE_CHOICES = (
--- backend/expenses/serializers.py
+++ backend/expenses/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Category, Transaction, Profile, CategoryLimit
+from django.db.models import Sum 
+from django.utils.timezone import now
+from decimal import Decimal
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -75,13 +78,85 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class CategoryLimitSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='category.name', read_only=True)
+
+    spent = serializers.SerializerMethodField()
+    remaining = serializers.SerializerMethodField()
+    percentage_used = serializers.SerializerMethodField
+    status = serializers.SerializerMethodField()    
     class Meta:
+        model = CategoryLimit
         fields = [
             'id',
             'category',
             'category_name',
-            'limit'
+            'limit',
+            'spent',
+            'remaining',
+            'percentage_used',
+            'status'
         ]
+    
+    def validate_category(self, value):
+        if value.type != "expense":
+            raise serializers.ValidationError(
+                "Only expense categories have limits"
+            )
+        return value
+    def validate(self, data):
+        user = self.context['request'].user
+        category = data.get('category')
+
+        queryset = CategoryLimit.objects.filter(
+            user = user,
+            category=category
+        )
+
+        if self.instance:
+            queryset = queryset.exclude(id=self.instance.id)
+        if queryset.exists():
+            raise serializers.ValidationError({
+                'category':'This category already has a limit set'
+            })
+        
+        return data
+    def get_current_month_expense(self, obj):
+        today = now()
+
+        total = Transaction.objects.filter(
+            user = obj.user,
+            category=obj.category,
+            type = 'expense',
+            date__year = today.year,
+            date__month = today.month
+        ).aggregate(total=Sum('amount'))['total']
+        return total or Decimal('0.00')
+    
+    def get_spent(self,obj):
+        return self.get_current_month_expense(obj)
+    
+    def get_remaining(self,obj):
+        spent = self.get_current_month_expense(obj)
+
+        if obj.limit is None:
+            return Decimal('0.00')
+        remaining = obj.limit - spent
+        return remaining
+    def get_percentage_used(self,obj):
+        spent = self.get_current_month_expense(obj)
+        if not obj.limit or obj.limit == 0:
+            return 0
+        percentage = (spent/obj.limit)*100
+
+        return round(percentage, 2)
+    def get_status(self, obj):
+        percentage = self.get_percentage_used(obj)
+
+        if percentage>=100:
+            return "exceeded"
+        elif percentage >80:
+            return "warning"
+        return "safe"
+    
 
 class TransactionSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='category.name', read_only=True)
